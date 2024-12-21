81d381fe3684cee0a7ce2030e334b65dab5f2d7f
Yash9891 <yash042002@gmail.com>
2024-12-21 21:50:52 +0530

    first commit

---
+++ 01datatype/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Godzilla");
+        System.out.print("LOPi");
+        System.out.println(3 + 4);
+        int num = 90;// 4byte
+        float num2 = 5.37897f;// 4 byte
+        short sh = 463;// 2 byte
+        long l = 454323l;// 8byte
+        boolean b = true;// 1 byte
+        double d = 9.688967;
+
+        System.out.println(num2 + num);
+        char ch = 'y';// 2 byte
+        System.out.print(ch);
+        System.out.println(b);
+
+    }
+
+}
