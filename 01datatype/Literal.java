aa5e6abccab6e26fee0e8179622e8f7e9f6c0824
Yash9891 <yash042002@gmail.com>
2024-12-22 09:28:19 +0530

    commit

---
+++ 01datatype/Literal.java
@@ -0,0 +1,10 @@
+public class Literal {
+    public static void main(String[] args) {
+        int num = 0x7E;
+        int num1 = 10_00_00_00;
+        char c = 'a';
+        System.out.print(num);
+        System.out.println(num1);
+        System.out.println(++c);
+    }
+}
---
+++ 02typeconversion/Conversion.java
@@ -0,0 +1,21 @@
+
+public class Conversion {
+    public static void main(String[] args) {
+
+        int a = 45;
+        byte b = (byte) a;
+        float fi = 45.56f;
+
+        int x = (int) fi;
+
+        System.out.println(b);
+        System.out.println(x);
+
+        byte a1 = 10;
+        byte b1 = 30;
+        int result = a1 * b1;
+        System.out.print(result);
+
+    }
+
+}
